7a5c146015bcbba2a4b274dce3cff6e237cd2288
Philip Pemberton <philpem@philpem.me.uk>
2014-03-18 01:27:15 +0000

    Update PTdecode to handle output from other Ptouch drivers

--- PTdecode/src/main.cpp
+++ PTdecode/src/main.cpp
@@ -2,9 +2,10 @@
  * ptdecode: P-touch PT-2450DX output decoder
  ****************************************************************************/
 
+#include <cctype>
 #include <cstdio>
 #include <exception>
-#include "CImg.h"
+#include <CImg.h>
 
 using namespace std;
 using namespace cimg_library;
@@ -13,7 +14,7 @@ using namespace cimg_library;
 const unsigned int PT_HEAD_WIDTH = 1024;
 
 // If defined, makes "blank row" blocks visible
-//#define MAKE_BLANK_ROWS_VISIBLE
+#define MAKE_BLANK_ROWS_VISIBLE
 
 // custom exception class for file read errors
 class EReadError : public exception {
@@ -39,154 +40,6 @@ unsigned char getNext() {
 	}
 }
 
-// Handler for graphics transfer mode 1
-void runGraphicsXferMode1()
-{
-	bool exit = false;
-	unsigned int cm = -1;
-	unsigned long xpos = 0;
-	unsigned long ypos = 0;
-	unsigned long ydim = 128;
-	CImg<unsigned char> img(0, 0, 0, 0, (unsigned char)0);
-
-	while (!exit) {
-		unsigned char ch = getNext();
-		unsigned int len = 0;
-		unsigned int rowpos = 0;
-		unsigned char row[PT_HEAD_WIDTH / 8];	// stores uncompressed row data
-
-		switch (ch) {
-			case 'M':			// Set compression mode
-				ch = getNext();
-				cm = ch;
-				printf("Set compression mode: 0x%02X", ch);
-				switch (cm) {
-					case 0x02:
-						printf(" (TIFF/Packbits)\n");
-						break;
-					default:
-						printf(" *** Unknown, assuming uncompressed ***\n");
-						cm = 1;
-						break;
-				}
-				break;
-
-			case 'Z':			// Blank raster line
-				// Increment x-position and resize the image
-				img.resize(xpos+1, ydim, 1, 1, 0, 0);
-
-				// Blank the new row
-				if (img.dimy() > 0) {
-//					printf("Clear row: x=%lu\n", xpos);
-					for (int i=0; i<img.dimy(); i++) {
-#ifdef MAKE_BLANK_ROWS_VISIBLE
-						img(xpos, i) = 128;
-#else
-						img(xpos, i) = 255;
-#endif
-					}
-				}
-
-				xpos++;
-				break;
-
-			case 'G':			// Graphics data row
-				// decode the length
-				ch = getNext();
-				len = (((int)getNext()) << 8) + ch;
-
-				// Is gfx payload compressed or uncompressed?
-				if (cm == 1) {
-					// Uncompressed. Read straight into the row buffer.
-					while (len > 0) {
-						row[rowpos++] = getNext(); len--;
-					}
-				} else {
-					// Decompress the gfx data
-					rowpos = 0;
-					while (len > 0) {
-						// get the prefix byte
-						ch = getNext(); len--;
-
-						// Is this a "run" (a single byte replicated) or a "copy"?
-						int runlen;
-						if (ch & 0x80) {
-							// MSB set, it's a run
-							runlen = 257 - ((int)ch);
-
-							// Get the byte to replicate, and replicate it into the o/p buffer
-							ch = getNext(); len--;
-							while (runlen-- > 0) {
-								row[rowpos++] = ch;
-							}
-						} else {
-							// MSB clear, it's a copy
-							runlen = ((int)ch) + 1;
-
-							// Copy N bytes from the input stream to the output
-							while (runlen-- > 0) {
-								row[rowpos++] = getNext();
-								len--;
-							}
-						}
-					}
-				}
-
-				// Row decode complete. row contains the image data, and rowpos
-				// contains its length in bytes. Now shuffle it into CImg...
-
-				// If image height is less than size of image row, then make the
-				// image taller.
-				if (((unsigned int)img.dimy()) < (rowpos * 8)) {
-					ydim = rowpos * 8;
-				} else {
-					ydim = img.dimy();
-				}
-
-				// Perform the Y resize if necessary, but also make Xdim=Xdim+1
-				img.resize(xpos+1, ydim, 1, 1, 0, 0);
-
-				img(xpos, ydim/2) = 128;
-
-				// Now copy the image data...
-				ypos = 0;
-				for (unsigned int byte=0; byte<rowpos; byte++) {
-					for (unsigned int bit=0; bit<8; bit++) {
-						if (row[byte] & (0x80>>bit)) {
-							img(xpos, ypos) = 0;
-						} else {
-							img(xpos, ypos) = 255;
-						}
-
-						// Increment y-position
-						ypos++;
-					}
-				}
-
-				// An entire row has been decoded. Increment x-position.
-				xpos++;
-				break;
-
-			case 0x0c:		// FF
-				printf("Formfeed: Print without label feed (job completed, more labels follow)\n");
-				exit = true;
-				break;
-
-			case 0x1a:		// Ctrl-Z
-				printf("Ctrl-Z:   Print with label feed    (job completed, no further labels)\n");
-				exit = true;
-				break;
-
-			default:			// Something else
-				printf("** Unrecognised command prefix in gfx mode: 0x%02x\n", ch);
-				break;
-		}
-	}
-
-	// Display the contents of the image
-	img.display();
-}
-
 // Parse an ESC i command
 void parse_esc_i()
 {
@@ -194,6 +47,11 @@ void parse_esc_i()
 	unsigned int tmpI;
 
 	switch (ch) {
+		case 'A':				// ESC i A: QL-specific command
+			ch = getNext();
+			printf("*** QL-SPECIFIC: ESC i A 0x%02X\n", ch);
+			break;
+
 		case 'B':				// ESC i B: Specify baud rate
 			tmpI = getNext();
 			ch = getNext();
@@ -221,7 +79,7 @@ void parse_esc_i()
 			tmpI = getNext();
 			ch = getNext();
 			tmpI += ((int)ch)*256;
-			printf("Set margin:\t%d dots", tmpI);
+			printf("Set margin:\t%d dots\n", tmpI);
 			break;
 
 		case 'K':				// ESC i K: Set expanded mode
@@ -240,14 +98,13 @@ void parse_esc_i()
 			printf("Set graphics transfer mode 0x%02X: ", ch);
 			if (ch == 1) {
 				printf("Raster graphics mode\n");
-				runGraphicsXferMode1();
 			} else {
 				printf("\n\tUnrecognised graphics transfer mode: remainder of data may be garbage.\n");
 			}
 			break;
 
 		default:
-			printf("Unrecognised cmnd: ESC i 0x%02X\n", ch);
+			printf("Unrecognised cmnd: ESC i 0x%02X [%c]\n", ch, ch);
 			break;
 	}
 }
@@ -274,6 +131,16 @@ void parse_esc()
 
 int main(int argc, char **argv)
 {
+	unsigned int cm = -1;
+	unsigned long xpos = 0;
+	unsigned long ypos = 0;
+	unsigned long ydim = 128;
+	CImg<unsigned char> img(0, 0, 0, 0, (unsigned char)0);
+	unsigned int len = 0;
+	unsigned int rowpos = 0;
+	unsigned char row[PT_HEAD_WIDTH / 8];	// stores uncompressed row data
+
+
 	// check params
 	if (argc != 2) {
 		// wrong!
@@ -298,17 +165,137 @@ int main(int argc, char **argv)
 				case 0x00:		// NULL
 					printf("Null\n");
 					break;
+
 				case 0x0c:		// FF
+					img.display("P-Touch Data [no feed]", false);
 					printf("Formfeed: Print without feed\n");
+					xpos = 0;
 					break;
+
 				case 0x1a:		// Ctrl-Z
+					img.display("P-Touch Data [label feed]", false);
 					printf("Ctrl-Z:   Print with label feed\n");
+					xpos = 0;
 					break;
+
 				case 0x1b:		// ESC
 					parse_esc();
 					break;
+
+				case 'M':			// Set compression mode
+					ch = getNext();
+					cm = ch;
+					printf("Set compression mode: 0x%02x", ch);
+					switch (cm) {
+						case 0x02:
+							printf(" (TIFF/PackBits)\n");
+							break;
+						default:
+							printf(" *** Unknown, assuming uncompressed ***\n");
+							cm = 1;
+							break;
+					}
+					break;
+
+				case 'Z':			// blank raster line
+					// Increment x-position and resize the image
+					img.resize(xpos+1, ydim, 1, 1, 0, 0);
+
+					// Blank the new row
+					if (img.height() > 0) {
+	//					printf("clear row: x=%lu\n", xpos);
+						for (int i=0; i<img.height(); i++) {
+#ifdef MAKE_BLANK_ROWS_VISIBLE
+							img(xpos, i) = 128;
+#else
+							img(xpos, i) = 255;
+#endif
+						}
+					}
+
+					xpos++;
+					break;
+
+				case 'G':			// Graphics data row
+					// decode the length
+					ch = getNext();
+					len = (((int)getNext()) << 8) + ch;
+
+					// Is gfx payload compressed or uncompressed?
+					if (cm == 1) {
+						// Uncompressed. Read straight into the row buffer.
+						while (len > 0) {
+							row[rowpos++] = getNext(); len--;
+						}
+					} else {
+						// Decompress the gfx data
+						rowpos = 0;
+						while (len > 0) {
+							// get the prefix byte
+							ch = getNext(); len--;
+
+							// Is this a "run" (a single byte replicated) or a "copy"?
+							int runlen;
+							if (ch & 0x80) {
+								// MSB set, it's a run
+								runlen = 257 - ((int)ch);
+
+								// Get the byte to replicate, and replicate it into the o/p buffer
+								ch = getNext(); len--;
+								while (runlen-- > 0) {
+									row[rowpos++] = ch;
+								}
+							} else {
+								// MSB clear, it's a copy
+								runlen = ((int)ch) + 1;
+
+								// Copy N bytes from the input stream to the output
+								while (runlen-- > 0) {
+									row[rowpos++] = getNext();
+									len--;
+								}
+							}
+						}
+					}
+
+					// Row decode complete. row contains the image data, and rowpos
+					// contains its length in bytes. Now shuffle it into CImg...
+
+					// If image height is less than size of image row, then make the
+					// image taller.
+					if (((unsigned int)img.height()) < (rowpos * 8)) {
+						ydim = rowpos * 8;
+					} else {
+						ydim = img.height();
+					}
+
+					// Perform the Y resize if necessary, but also make Xdim=Xdim+1
+					img.resize(xpos+1, ydim, 1, 1, 0, 0);
+
+					img(xpos, ydim/2) = 128;
+
+					// Now copy the image data...
+					ypos = 0;
+					for (unsigned int byte=0; byte<rowpos; byte++) {
+						for (unsigned int bit=0; bit<8; bit++) {
+							if (row[byte] & (0x80>>bit)) {
+								img(xpos, ypos) = 0;
+							} else {
+								img(xpos, ypos) = 255;
+							}
+
+							// Increment y-position
+							ypos++;
+						}
+					}
+
+					// An entire row has been decoded. Increment x-position.
+					xpos++;
+					break;
+
+
 				default:
-					printf("Unrecognised cmnd: 0x%02X\n", ch);
+					printf("Unrecognised cmnd: 0x%02X [%c]\n", ch, ch);
 					break;
 			}
 		}
